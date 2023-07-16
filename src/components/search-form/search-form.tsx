e60526bf070a529cd65bec47720182f4ad1da921
Natalia <aproken@yandex.ru>
2023-07-16 15:18:17 +0300

    фокус залочен

--- src/components/search-form/search-form.tsx
+++ src/components/search-form/search-form.tsx
@@ -15,7 +15,7 @@ function SearchForm(): JSX.Element {
   const handleInputChange = (evt: ChangeEvent<HTMLInputElement>) => {
     const value = evt.target.value;
     setSearchTerm(value);
-    setFocusedIndex(-1);
+    setFocusedIndex(0);
   };
 
   const resetSearch = () => {
@@ -31,10 +31,12 @@ function SearchForm(): JSX.Element {
     if (event.key === 'ArrowDown') {
       event.preventDefault();
       setFocusedIndex((prevIndex) => (prevIndex < resultNames.length - 1 ? prevIndex + 1 : 0));
-    } else if (event.key === 'ArrowUp') {
+    } else
+    if (event.key === 'ArrowUp') {
       event.preventDefault();
       setFocusedIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : resultNames.length - 1));
-    } else if (event.key === 'Tab') {
+    } else
+    if (event.key === 'Tab') {
       if (listRef.current && listRef.current.contains(document.activeElement)) {
         event.preventDefault();
         if (event.shiftKey) {
@@ -42,32 +44,30 @@ function SearchForm(): JSX.Element {
         } else {
           setFocusedIndex((prevIndex) => (prevIndex < resultNames.length - 1 ? prevIndex + 1 : 0));
         }
-      } else {
-        resetSearch();
       }
     }
   };
 
   return (
     <div className={ classNames('form-search', { 'list-opened ': searchTerm && resultNames.length}) }>
-      <form>
-        <label>
-          <svg className="form-search__icon" width="16" height="16" aria-hidden="true">
-            <use xlinkHref="#icon-lens"></use>
-          </svg>
-          <input
-            className="form-search__input"
-            type="text"
-            autoComplete="off"
-            placeholder="Поиск по сайту"
-            value={ searchTerm }
-            onChange={ handleInputChange }
-            onKeyDown={ handleKeyDown }
-          />
-        </label>
-        {
-          searchTerm && (
-            <FocusLock>
+      <FocusLock>
+        <form>
+          <label>
+            <svg className="form-search__icon" width="16" height="16" aria-hidden="true">
+              <use xlinkHref="#icon-lens"></use>
+            </svg>
+            <input
+              className="form-search__input"
+              type="text"
+              autoComplete="off"
+              placeholder="Поиск по сайту"
+              value={ searchTerm }
+              onChange={ handleInputChange }
+              onKeyDown={ handleKeyDown }
+            />
+          </label>
+          {
+            searchTerm && (
               <ul ref={ listRef } className="form-search__select-list">
                 {
                   resultNames.map((camera, index) => (
@@ -81,16 +81,16 @@ function SearchForm(): JSX.Element {
                   ))
                 }
               </ul>
-            </FocusLock>
-          )
-        }
-      </form>
-      <button className="form-search__reset" type="reset" onClick={resetSearch}>
-        <svg width="10" height="10" aria-hidden="true">
-          <use xlinkHref="#icon-close"></use>
-        </svg>
-        <span className="visually-hidden">Сбросить поиск</span>
-      </button>
+            )
+          }
+        </form>
+        <button className="form-search__reset" type="reset" onClick={resetSearch}>
+          <svg width="10" height="10" aria-hidden="true">
+            <use xlinkHref="#icon-close"></use>
+          </svg>
+          <span className="visually-hidden">Сбросить поиск</span>
+        </button>
+      </FocusLock>
     </div>
   );
 }
