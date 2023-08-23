deadd036c0afd8f3595473964d2f20f7c622b3b6
Natalia <aproken@yandex.ru>
2023-08-23 16:30:21 +0300

    правкат блока цены

--- src/components/price-block/price-block.test.tsx
+++ src/components/price-block/price-block.test.tsx
@@ -1,17 +1,357 @@
-import { render } from '@testing-library/react';
+import { act, fireEvent, render, screen } from '@testing-library/react';
 import PriceBlock from './price-block';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import HistoryRouter from '../../components/history-route/history-route';
 
+const history = createBrowserHistory();
+history.push('/');
 
 describe('PriceBlock', ()=> {
-  it('Принимает параметры', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+  beforeEach(() => {
+    history.replace('/');
+  });
+
+  it('Отрисовка пустой формы', () => {
+    const minPrice = 5000;
+    const maxPrice = 100000;
+
+    history.push('/');
+    render(
+      <HistoryRouter history={ history }>
+        <PriceBlock minPrice={ minPrice } maxPrice={ maxPrice }/>
+      </HistoryRouter>
+    );
+
+    const inputMin = screen.getByLabelText('Цена от');
+    const inputMax = screen.getByLabelText('Цена до');
+
+    expect(inputMin.getAttribute('placeholder')).toBe('5000');
+    expect(inputMin.getAttribute('value')).toBe('');
+    expect(inputMax.getAttribute('placeholder')).toBe('100000');
+    expect(inputMax.getAttribute('value')).toBe('');
+  });
+
+  it('Отрисовка заполненой формы', () => {
+    const minPrice = 5000;
+    const maxPrice = 100000;
+
+    history.push('/?price_gte=5500&price_lte=8000');
+    render(
+      <HistoryRouter history={ history }>
+        <PriceBlock minPrice={ minPrice } maxPrice={ maxPrice }/>
+      </HistoryRouter>
+    );
+
+    const inputMin = screen.getByLabelText('Цена от');
+    const inputMax = screen.getByLabelText('Цена до');
+
+    expect(inputMin.getAttribute('placeholder')).toBe('5000');
+    expect(inputMin.getAttribute('value')).toBe('5500');
+    expect(inputMax.getAttribute('placeholder')).toBe('100000');
+    expect(inputMax.getAttribute('value')).toBe('8000');
+  });
+
+  it('Смена URL сбрасывает состояние формы', () => {
+    const minPrice = 5000;
+    const maxPrice = 100000;
+
+    history.push('/?price_gte=5500&price_lte=8000');
+    render(
+      <HistoryRouter history={ history }>
+        <PriceBlock minPrice={ minPrice } maxPrice={ maxPrice }/>
+      </HistoryRouter>
+    );
+
+    act(() => history.push('/'));
+
+    const inputMin = screen.getByLabelText('Цена от');
+    const inputMax = screen.getByLabelText('Цена до');
+
+    expect(inputMin.getAttribute('placeholder')).toBe('5000');
+    expect(inputMin.getAttribute('value')).toBe('');
+    expect(inputMax.getAttribute('placeholder')).toBe('100000');
+    expect(inputMax.getAttribute('value')).toBe('');
+  });
+
+  it('Обнуление URL сбрасывает состояние формы', () => {
+    const minPrice = 5000;
+    const maxPrice = 100000;
+
+    history.push('/');
+    render(
+      <HistoryRouter history={ history }>
+        <PriceBlock minPrice={ minPrice } maxPrice={ maxPrice }/>
+      </HistoryRouter>
+    );
+
+    const inputMin = screen.getByLabelText('Цена от');
+    const inputMax = screen.getByLabelText('Цена до');
+
+    fireEvent.change(inputMin, {target: {value: '6000'}});
+    //fireEvent.blur(inputMin);
+
+    act(() => {
+      jest.runAllTimers();
+    });
+
+    act(() => history.push('/'));
+
+    expect(inputMin.getAttribute('placeholder')).toBe('5000');
+    expect(inputMin.getAttribute('value')).toBe('');
+    expect(inputMax.getAttribute('placeholder')).toBe('100000');
+    expect(inputMax.getAttribute('value')).toBe('');
+  });
+
+  it('MinValue сохраняется в URL по таймауту', () => {
     const minPrice = 5000;
     const maxPrice = 100000;
 
     render(
-      <Router>
+      <HistoryRouter history={ history }>
         <PriceBlock minPrice={ minPrice } maxPrice={ maxPrice }/>
-      </Router>
+      </HistoryRouter>
     );
+
+    const inputMin = screen.getByLabelText('Цена от');
+
+    fireEvent.change(inputMin, {target: {value: '6000'}});
+    // fireEvent.blur(inputMin);
+
+    act(() => {
+      jest.runAllTimers();
+    });
+    expect(window.location.search).toBe('?price_gte=6000');
+    expect(inputMin.getAttribute('value')).toBe('6000');
+  });
+
+  it('MinValue сохраняется в URL по onBlur', () => {
+    const minPrice = 5000;
+    const maxPrice = 100000;
+
+    render(
+      <HistoryRouter history={ history }>
+        <PriceBlock minPrice={ minPrice } maxPrice={ maxPrice }/>
+      </HistoryRouter>
+    );
+
+    const inputMin = screen.getByLabelText('Цена от');
+
+    fireEvent.change(inputMin, {target: {value: '6000'}});
+    fireEvent.blur(inputMin);
+
+    act(() => {
+      jest.runAllTimers();
+    });
+    expect(window.location.search).toBe('?price_gte=6000');
+    expect(inputMin.getAttribute('value')).toBe('6000');
+  });
+
+  it('MaxValue сохраняется в URL', () => {
+    const minPrice = 5000;
+    const maxPrice = 100000;
+
+    history.push('/');
+    render(
+      <HistoryRouter history={ history }>
+        <PriceBlock minPrice={ minPrice } maxPrice={ maxPrice }/>
+      </HistoryRouter>
+    );
+
+    const inputMax = screen.getByLabelText('Цена до');
+
+    fireEvent.change(inputMax, {target: {value: '8000'}});
+    fireEvent.blur(inputMax);
+
+    act(() => {
+      jest.runAllTimers();
+    });
+
+    expect(window.location.search).toBe('?price_lte=8000');
+    expect(inputMax.getAttribute('value')).toBe('8000');
+  });
+
+
+  it('value < MinValue = MinValue', () => {
+    const minPrice = 5000;
+    const maxPrice = 100000;
+
+    expect(window.location.search).toBe('');
+    render(
+      <HistoryRouter history={ history }>
+        <PriceBlock minPrice={ minPrice } maxPrice={ maxPrice }/>
+      </HistoryRouter>
+    );
+
+    const inputMin = screen.getByLabelText('Цена от');
+    const inputMax = screen.getByLabelText('Цена до');
+
+    fireEvent.change(inputMin, {target: {value: '1'}});
+    fireEvent.blur(inputMin);
+
+    act(() => {
+      jest.runAllTimers();
+    });
+
+    expect(inputMin.getAttribute('value')).toBe('');
+    expect(inputMin.getAttribute('placeholder')).toBe('5000');
+    expect(inputMax.getAttribute('value')).toBe('');
+    expect(inputMax.getAttribute('placeholder')).toBe('100000');
+    expect(window.location.search).toBe('');
+  });
+
+  it('value > MaxValue = MaxValue', () => {
+    const minPrice = 5000;
+    const maxPrice = 100000;
+
+    render(
+      <HistoryRouter history={ history }>
+        <PriceBlock minPrice={ minPrice } maxPrice={ maxPrice }/>
+      </HistoryRouter>
+    );
+
+    const inputMin = screen.getByLabelText('Цена от');
+    const inputMax = screen.getByLabelText('Цена до');
+
+    fireEvent.change(inputMax, {target: {value: '999999'}});
+    fireEvent.blur(inputMax);
+
+    act(() => {
+      jest.runAllTimers();
+    });
+
+    expect(inputMin.getAttribute('value')).toBe('');
+    expect(inputMin.getAttribute('placeholder')).toBe('5000');
+    expect(inputMax.getAttribute('value')).toBe('');
+    expect(inputMax.getAttribute('placeholder')).toBe('100000');
+    expect(window.location.search).toBe('');
+  });
+
+  it('MaxValue не может быть меньше MinValue', () => {
+    const minPrice = 5000;
+    const maxPrice = 100000;
+
+    render(
+      <HistoryRouter history={ history }>
+        <PriceBlock minPrice={ minPrice } maxPrice={ maxPrice }/>
+      </HistoryRouter>
+    );
+
+    const inputMin = screen.getByLabelText('Цена от');
+    const inputMax = screen.getByLabelText('Цена до');
+
+    fireEvent.change(inputMin, {target: {value: '6000'}});
+    fireEvent.blur(inputMin);
+    fireEvent.change(inputMax, {target: {value: '5500'}});
+    fireEvent.blur(inputMax);
+
+    act(() => {
+      jest.runAllTimers();
+    });
+
+    expect(inputMin.getAttribute('value')).toBe('6000');
+    expect(inputMin.getAttribute('placeholder')).toBe('5000');
+    expect(inputMax.getAttribute('value')).toBe('6000');
+    expect(inputMax.getAttribute('placeholder')).toBe('100000');
+
+    const urlParams = new URLSearchParams(window.location.search);
+    expect(urlParams.get('price_lte')).toBe('6000');
+    expect(urlParams.get('price_gte')).toBe('6000');
+  });
+
+  it('MinValue не может быть больше MaxValue', () => {
+    const minPrice = 5000;
+    const maxPrice = 100000;
+
+    render(
+      <HistoryRouter history={ history }>
+        <PriceBlock minPrice={ minPrice } maxPrice={ maxPrice }/>
+      </HistoryRouter>
+    );
+
+    const inputMin = screen.getByLabelText('Цена от');
+    const inputMax = screen.getByLabelText('Цена до');
+
+    fireEvent.change(inputMax, {target: {value: '6000'}});
+    fireEvent.blur(inputMax);
+    fireEvent.change(inputMin, {target: {value: '7000'}});
+    fireEvent.blur(inputMin);
+
+    act(() => {
+      jest.runAllTimers();
+    });
+
+    expect(inputMin.getAttribute('value')).toBe('6000');
+    expect(inputMin.getAttribute('placeholder')).toBe('5000');
+    expect(inputMax.getAttribute('value')).toBe('6000');
+    expect(inputMax.getAttribute('placeholder')).toBe('100000');
+    const urlParams = new URLSearchParams(window.location.search);
+    expect(urlParams.get('price_lte')).toBe('6000');
+    expect(urlParams.get('price_gte')).toBe('6000');
+  });
+
+  it('MinValue и MaxValue равный Null не сохраняется в URL', () => {
+    const minPrice = 5000;
+    const maxPrice = 100000;
+
+    history.push('/?price_gte=5500&price_lte=8000');
+    render(
+      <HistoryRouter history={ history }>
+        <PriceBlock minPrice={ minPrice } maxPrice={ maxPrice }/>
+      </HistoryRouter>
+    );
+
+    const inputMin = screen.getByLabelText('Цена от');
+    const inputMax = screen.getByLabelText('Цена до');
+
+    fireEvent.change(inputMin, {target: {value: ''}});
+    fireEvent.blur(inputMin);
+    fireEvent.change(inputMax, {target: {value: ''}});
+    fireEvent.blur(inputMax);
+
+    act(() => {
+      jest.runAllTimers();
+    });
+
+    expect(inputMin.getAttribute('value')).toBe('');
+    expect(inputMin.getAttribute('placeholder')).toBe('5000');
+    expect(inputMax.getAttribute('value')).toBe('');
+    expect(inputMax.getAttribute('placeholder')).toBe('100000');
+    expect(window.location.search).toBe('');
+  });
+
+  it('MinValue и MaxValue равный Props не сохраняется в URL', () => {
+    const minPrice = 5000;
+    const maxPrice = 100000;
+
+    history.push('/?price_gte=5500&price_lte=8000');
+    render(
+      <HistoryRouter history={ history }>
+        <PriceBlock minPrice={ minPrice } maxPrice={ maxPrice }/>
+      </HistoryRouter>
+    );
+
+    const inputMin = screen.getByLabelText('Цена от');
+    const inputMax = screen.getByLabelText('Цена до');
+
+    fireEvent.change(inputMin, {target: {value: '5000'}});
+    fireEvent.blur(inputMin);
+    fireEvent.change(inputMax, {target: {value: '100000'}});
+    fireEvent.blur(inputMax);
+
+    act(() => {
+      jest.runAllTimers();
+    });
+
+    expect(inputMin.getAttribute('value')).toBe('');
+    expect(inputMin.getAttribute('placeholder')).toBe('5000');
+    expect(inputMax.getAttribute('value')).toBe('');
+    expect(inputMax.getAttribute('placeholder')).toBe('100000');
+    expect(window.location.search).toBe('');
   });
 });
--- src/components/price-block/price-block.tsx
+++ src/components/price-block/price-block.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import useState from 'react-usestateref';
 import { QueryParameterPriceBlock } from '../../const';
-import { useDebounce } from '../../hooks/useDebounce';
 
 type PriceBlockProps = {
   minPrice: number;
@@ -9,39 +9,90 @@ type PriceBlockProps = {
 }
 
 type PriceState = {
-  minPrice: number | null;
-  maxPrice: number | null;
+  [QueryParameterPriceBlock.MinPrice]: number | null;
+  [QueryParameterPriceBlock.MaxPrice]: number | null;
 }
+
+const normolizeUrl = (url: URLSearchParams) => {
+  const filterEntries = Array.from(url.entries()).filter(([_k,v]) => ![null, 'null', ''].includes(v));
+  return new URLSearchParams([...filterEntries]);
+};
 
 function PriceBlock({ minPrice, maxPrice }: PriceBlockProps): JSX.Element {
   const [searchParams, setSearchParams] = useSearchParams();
+  let timoutMin: ReturnType<typeof setTimeout> ;
+  let timoutMax: ReturnType<typeof setTimeout> ;
 
   const minPriceParams = searchParams.get(QueryParameterPriceBlock.MinPrice);
   const maxPriceParams = searchParams.get(QueryParameterPriceBlock.MaxPrice);
 
-  let minPriceValue = minPrice;
-  if(minPriceParams) {
-    minPriceValue = parseInt(minPriceParams, 10);
-  }
+  const minPriceValue = parseInt(minPriceParams || '', 10) || null;
+  const maxPriceValue = parseInt(maxPriceParams || '', 10) || null;
 
-  let maxPriceValue = maxPrice;
-  if(maxPriceParams) {
-    maxPriceValue = parseInt(maxPriceParams, 10);
-  }
-
-  const [price, setPrice] = useState<PriceState>({
-    minPrice: null,
-    maxPrice: null
+  const [price, setPrice, priceRef] = useState<PriceState>({
+    [QueryParameterPriceBlock.MinPrice]: null,
+    [QueryParameterPriceBlock.MaxPrice]: null
   });
 
-  // useEffect(() => {
-  //   setPrice({
-  //     minPrice: minPriceValue,
-  //     maxPrice: maxPriceValue,
-  //   });
-  // } , [minPriceValue, maxPriceValue]);
+  const process = (valueType: QueryParameterPriceBlock) => {
+    const currentPrice = priceRef.current;
 
-  const debouncedValue = useDebounce(price, 2000);
+    const alterValueType = (valueType === 'price_gte') ?
+      QueryParameterPriceBlock.MaxPrice
+      :
+      QueryParameterPriceBlock.MinPrice;
+
+    const value = currentPrice[valueType];
+    const alterValue = currentPrice[alterValueType];
+
+    //rules
+    // Если значение меньше minPrice
+    if ((value || -Infinity) < minPrice) {
+      currentPrice[valueType] = minPrice;
+    }
+    // Если значение больше maxPrice
+    if ((value || Infinity) > maxPrice) {
+      currentPrice[valueType] = maxPrice;
+    }
+
+    if (
+      value && alterValue &&
+      valueType === QueryParameterPriceBlock.MaxPrice &&
+      value < alterValue
+    ) {
+      currentPrice[valueType] = alterValue;
+    }
+
+    if (
+      value && alterValue &&
+      valueType === QueryParameterPriceBlock.MinPrice &&
+      value > alterValue
+    ) {
+      currentPrice[valueType] = alterValue;
+    }
+
+    if (
+      valueType === QueryParameterPriceBlock.MaxPrice &&
+      (value === maxPrice || value === null)
+    ) {
+      currentPrice[valueType] = null;
+    }
+    if (
+      valueType === QueryParameterPriceBlock.MinPrice &&
+      (value === minPrice || value === null)
+    ) {
+      currentPrice[valueType] = null;
+    }
+    setSearchParams((prevParams) => {
+      prevParams.set(
+        QueryParameterPriceBlock.MaxPrice, String(currentPrice[QueryParameterPriceBlock.MaxPrice])
+      );
+      prevParams.set(
+        QueryParameterPriceBlock.MinPrice, String(currentPrice[QueryParameterPriceBlock.MinPrice])
+      );
+      return normolizeUrl(prevParams);
+    });
+  };
 
   const handleMinPriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
@@ -50,19 +101,25 @@ function PriceBlock({ minPrice, maxPrice }: PriceBlockProps): JSX.Element {
       return;
     }
 
-    const newValue = parseInt(value, 10);
-
-    if(newValue > maxPriceValue) {
-      setPrice((prevPrice) => ({
-        ...prevPrice,
-        minPrice:maxPriceValue
-      }));
+    if (timoutMin) {
+      clearTimeout(timoutMin);
     } else {
+      timoutMin = setTimeout(() => process(QueryParameterPriceBlock.MinPrice), 2000);
+    }
+
+    if (!value) {
       setPrice((prevPrice) => ({
         ...prevPrice,
-        minPrice:newValue
+        [QueryParameterPriceBlock.MinPrice]: null
       }));
+      return ;
     }
+
+    const newValue = parseInt(value, 10);
+    setPrice((prevPrice) => ({
+      ...prevPrice,
+      [QueryParameterPriceBlock.MinPrice]:newValue
+    }));
   };
 
   const handleMaxPriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -72,80 +129,78 @@ function PriceBlock({ minPrice, maxPrice }: PriceBlockProps): JSX.Element {
       return;
     }
 
+    if (timoutMin) {
+      clearTimeout(timoutMax);
+    } else {
+      timoutMax = setTimeout(() => process(QueryParameterPriceBlock.MaxPrice), 2500);
+    }
+
+    if (!value) {
+      setPrice((prevPrice) => ({
+        ...prevPrice,
+        [QueryParameterPriceBlock.MaxPrice]: null
+      }));
+      return ;
+    }
+
     const newValue = parseInt(value, 10);
 
-
-    if(newValue > maxPrice) {
-      setPrice((prevPrice) => ({
-        ...prevPrice,
-        maxPrice: maxPrice
-      }));
-    } else {
-      setPrice((prevPrice) => ({
-        ...prevPrice,
-        maxPrice: newValue
-      }));
-    }
+    setPrice((prevPrice) => ({
+      ...prevPrice,
+      [QueryParameterPriceBlock.MaxPrice]: newValue
+    }));
   };
 
+  // Выставляем значение формы если у нас есть сохраненные данные в URL
   useEffect(() => {
     if (
-      debouncedValue.maxPrice?.toString() === maxPriceParams &&
-      debouncedValue.minPrice?.toString() === minPriceParams
-    ) {
-      return;
+      minPriceValue === priceRef.current[QueryParameterPriceBlock.MinPrice] &&
+      maxPriceValue === priceRef.current[QueryParameterPriceBlock.MaxPrice]
+    ){
+      // Если состония эквивалентны - завершаем
+      return ;
     }
-
-    let maxPriceToPersist = debouncedValue.maxPrice;
-    if (isNaN(debouncedValue.maxPrice || NaN) || (debouncedValue.maxPrice || NaN) > maxPrice) {
-      maxPriceToPersist = maxPrice;
+    const update = {} as Partial<PriceState>;
+    if (minPriceValue !== price[QueryParameterPriceBlock.MinPrice] ) {
+      update[QueryParameterPriceBlock.MinPrice] = minPriceValue;
     }
-
-    let minPriceToPersist = debouncedValue.minPrice;
-    if (isNaN(debouncedValue.minPrice || NaN) || (debouncedValue.minPrice || NaN) < minPriceValue) {
-      minPriceToPersist = minPriceValue;
+    if (maxPriceValue !== price[QueryParameterPriceBlock.MaxPrice] ) {
+      update[QueryParameterPriceBlock.MaxPrice] = maxPriceValue;
     }
-
-    if (
-      (debouncedValue.maxPrice !== null && debouncedValue.minPrice !== null) &&
-      (debouncedValue.maxPrice < debouncedValue.minPrice)
-    ) {
-      maxPriceToPersist = debouncedValue.minPrice;
-    }
-
-    // setPrice({
-    //   minPrice: minPriceToPersist,
-    //   maxPrice: maxPriceToPersist,
-    // });
-
-    setSearchParams((prevParams) => {
-      prevParams.set(QueryParameterPriceBlock.MaxPrice, String(maxPriceToPersist));
-      prevParams.set(QueryParameterPriceBlock.MinPrice, String(minPriceToPersist));
-      return prevParams;
-    });
-  }, [debouncedValue]);
+    setPrice({...price, ...update});
+  }, [minPriceValue, maxPriceValue]);
 
   return (
     <div className="catalog-filter__price-range">
       <div className="custom-input">
         <label>
           <input
+            aria-label="Цена от"
             onChange={ handleMinPriceChange }
             type="number"
             name={ QueryParameterPriceBlock.MinPrice }
-            value={ price.minPrice || ''}
+            value={ price[QueryParameterPriceBlock.MinPrice] || ''}
             placeholder={ `${ minPrice }`}
+            onBlur={() => {
+              clearTimeout(timoutMin);
+              process(QueryParameterPriceBlock.MinPrice);
+            }}
           />
         </label>
       </div>
       <div className="custom-input">
         <label>
           <input
+            aria-label="Цена до"
             onChange={ handleMaxPriceChange }
             type="number"
             name={ QueryParameterPriceBlock.MaxPrice }
-            value={ price.maxPrice || ''}
+            value={ price[QueryParameterPriceBlock.MaxPrice] || ''}
             placeholder={ `${ maxPrice }`}
+            onBlur={() => {
+              clearTimeout(timoutMax);
+              process(QueryParameterPriceBlock.MaxPrice);
+            }}
           />
         </label>
       </div>
